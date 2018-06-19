4783ca225c8c60a7947a225682426f256d2fe2c6
Blaine Smith <Smith.blaine1993@gmail.com>
2018-06-19 11:44:40 -0500

    method check

--- PlayThrough/src/com/company/Main.java
+++ PlayThrough/src/com/company/Main.java
@@ -105,7 +105,7 @@ public class Main {
 
     private static void GoodBye()
     {
-        System.out.println("Goodbye for now");
+        System.out.println("Goodbye for now or is it?");
     }
 
     private static String End()
